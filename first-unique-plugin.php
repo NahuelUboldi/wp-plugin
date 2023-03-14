9374a9e600b1656c520abfddc6de9b8ba795847f
NahuelUboldi <nahueluboldi@gmail.com>
2023-03-14 15:50:21 -0300

    settings screen

--- first-unique-plugin.php
+++ first-unique-plugin.php
@@ -10,6 +10,22 @@ Author: URI https://nahueluboldi.com
 class WordCountAndTimePlugin {
   function __construct() {
     add_action( 'admin_menu', array($this,'adminPage'));
+    add_action( 'admin_init', array($this,'settings'));
+  }
+
+  function settings() {
+    add_settings_section( 'wcp_first_section', null, null, 'word-count-settings-page' );
+    add_settings_field('wcp_location','Display Location',array($this,'locationHTML'),'word-count-settings-page', 'wcp_first_section');
+    register_setting('wordcountplugin','wcp_location',array('sanitize_callback' => 'sanitize_text_field', 'default' => '0 '));
+  }
+
+  function locationHTML() { 
+    ?>
+    <select name="wcp_location">
+      <option value="0">Beginning of post</option>
+      <option value="1">End of post</option>
+    </select>
+    <?php
   }
 
   function adminPage() {
@@ -18,9 +34,19 @@ class WordCountAndTimePlugin {
 
   function ourHTML() {
     ?>
+
     <div class="wrap">
-    <h1>Word Count Settings</h1>
+      <h1>Word Count Settings</h1>
+      <form action="options.php" method="POST">
+        <?php
+        settings_fields( 'wordcountplugin' ); 
+        do_settings_sections( 'word-count-settings-page');
+        submit_button();
+        ?>
+
+      </form>
     </div>
+
     <?php
   }
 }
